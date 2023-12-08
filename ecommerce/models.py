d100edc173e1c018e42d6e984e3dac5db8d91c81
Mohammadreza-Alizadeh <alizadehmohamadreza641@gmail.com>
2023-12-08 21:03:04 +0330

    Created Order model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -5,7 +5,6 @@ from accounts.models import Profile
 # Create your models here.
 class Product(models.Model):
 
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='products')
     title = models.CharField(max_length=128)
     description = models.TextField(blank=True)
     stock = models.IntegerField(default=0)
@@ -14,7 +13,19 @@ class Product(models.Model):
     def __str__(self):
         return f'{self.title}'
     
+
     def decrease_stock(self ,by=1):
         if self.stock < by :
-            return 
-        self.stock -= by
+            return None
+        self.stock -= by        
+        return self.stock
+
+
+class Order(models.Model):
+    
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='orders')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, related_name='orders')
+    created_date = models.DateField(auto_now_add=True)
+
+
+
